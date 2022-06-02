347f6c0741e5b02f129b7311168caefceb117771
Marty <jmjmjm373@gmail.com>
2022-06-02 17:10:01 +0800

    Add functional math operations.

---
+++ script.js
@@ -0,0 +1,16 @@
+function add(previous, current) {
+    return previous + current;
+}
+
+function subtract(previous, current) {
+    return previous - current;
+}
+
+function multiply(previous, current) {
+    return previous * current;
+}
+
+function divide(previous, current) {
+    return previous / current;
+}
+
